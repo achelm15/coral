c4210d48bcda94ac1d437679f8ce323b0ea70cea
achelm15 <achelm@rams.colostate.edu>
2022-02-08 13:27:31 -0700

    change

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -7,6 +7,7 @@ import os
 import sys
 from pathlib import Path
 import cv2
+import time
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -21,15 +22,79 @@ def process_outs(prediction, conf_thres=25, iou_thres=45, classes=None, agnostic
     print(prediction)
     nc = prediction.shape[2] - 5  # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
-    print(prediction[..., 4]>25)
-    print(nc, "THIS IS NC")
-    count = 0
-    for x in xc[0]:
-        if x == True:
-            count = count +1
-        # print(x, "asdfasdf")
-    print(count)
+
+    output = np.zeros((0,6))*prediction.shape[0]
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+        x = x[xc[xi]]  # confidence
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+        print(type(x[:,:4]))
+
+        # # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        # box = xywh2xyxy(x[:, :4])
+
+        # # Detections matrix nx6 (xyxy, conf, cls)
+        # if multi_label:
+        #     i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+        #     x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+        # else:  # best class only
+        #     conf, j = x[:, 5:].max(1, keepdim=True)
+        #     x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+
+        # # Filter by class
+        # if classes is not None:
+        #     x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+        # # Apply finite constraint
+        # # if not torch.isfinite(x).all():
+        # #     x = x[torch.isfinite(x).all(1)]
+
+        # # Check shape
+        # n = x.shape[0]  # number of boxes
+        # if not n:  # no boxes
+        #     continue
+        # elif n > max_nms:  # excess boxes
+        #     x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+
+        # # Batched NMS
+        # c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        # boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        # print(boxes.shape, scores.shape, "999999999999999999999999999999999999")
+        # i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        # if i.shape[0] > max_det:  # limit detections
+        #     i = i[:max_det]
+        # if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+        #     # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+        #     iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+        #     weights = iou * scores[None]  # box weights
+        #     x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+        #     if redundant:
+        #         i = i[iou.sum(1) > 1]  # require redundancy
+
+        # output[xi] = x[i]
+        # if (time.time() - t) > time_limit:
+        #     print(f'WARNING: NMS time limit {time_limit}s exceeded')
+        #     break  # time limit exceeded
+
+
+
     return
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, np.array) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
 
 def process_image(img, imgsz):
     image = np.array(img.resize((imgsz, imgsz)), dtype="int8")
