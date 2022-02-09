627ba188612e9234206ea5d9d2ef02697f9d35df
achelm15 <achelm@rams.colostate.edu>
2022-02-09 11:24:41 -0700

    change

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -131,8 +131,8 @@ def process_outs(prediction, conf_thres=25, iou_thres=.45, classes=None, agnosti
         #         i = i[iou.sum(1) > 1]  # require redundancy
 
         k = [x[u] for u in i]
-        print(k)
-        output[xi] = k[0]
+        print(k[0])
+        # output[xi] = k[0]
         # print(output)
         if (time.time() - t) > time_limit:
             print(f'WARNING: NMS time limit {time_limit}s exceeded')
