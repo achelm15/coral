e08cebb579f33cd9290c9073d86d9509b73c64f1
achelm15 <achelm@rams.colostate.edu>
2022-02-09 11:01:17 -0700

    change

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -118,15 +118,11 @@ def process_outs(prediction, conf_thres=25, iou_thres=45, classes=None, agnostic
                 if test[0]:
                     deletions.append(index)
             print(deletions)
-            # for i in range(0, len(deletions)):
-            #     x = np.delete(x,deletions[1]-i,0)
             x = np.array([x[item].tolist() for item in range(0,len(x)) if item not in deletions])
-            print("Asdfasdf")
-            print(x.shape)
-            print(x)
-        # # Filter by class
-        # if classes is not None:
-        #     x = x[(x[:, 5:6] == np.array(classes)).any(1)]
+            
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == np.array(classes)).any(1)]
 
 
         # # Check shapes
