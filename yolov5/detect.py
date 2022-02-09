007fe4367c6534b79b8ecdc2b5d76bfe9af0e792
achelm15 <achelm@rams.colostate.edu>
2022-02-09 15:03:38 -0700

    test

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -46,7 +46,7 @@ def detect_image(image, interpreter, imgsz, data, pathname):
         print(i,det)
         print(img.shape)
         print(im0.shape)
-        print(img.shape[1:2])
+        print(img.shape[1:3])
         det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
         annotator = Annotator(im0, line_width=3, example=str(data))
         for *xyxy, conf, cls in reversed(det):
