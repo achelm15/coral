9569584e1666df220471024ffc03dd103a1e3a17
achelm15 <achelm@rams.colostate.edu>
2022-02-16 11:38:21 -0700

    remove window show

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -1,4 +1,4 @@
-import tflite_runtime.interpreter as tflite
+# import tflite_runtime.interpreter as tflite
 from PIL import Image
 import datetime
 import numpy as np
@@ -128,19 +128,19 @@ def detect_video(video, interpreter, imgsz, data, conf):
 def run(weights=ROOT / 'yolov5s.pt', source=ROOT / 'data/images', imgsz=256, data="datasets/LPCV.yaml", conf=0.25):
     model_path, source, imgsz, data= opt.weights, opt.source, opt.imgsz, opt.data
     data = get_data_dict(data)['names']
-    interpreter = tflite.Interpreter(model_path)
-    interpreter = tflite.Interpreter(model_path, experimental_delegates=[tflite.load_delegate("libedgetpu.so.1")])
-    # import tensorflow as tf
+    # interpreter = tflite.Interpreter(model_path)
+    # interpreter = tflite.Interpreter(model_path, experimental_delegates=[tflite.load_delegate("libedgetpu.so.1")])
+    import tensorflow as tf
 
-    # interpreter = tf.lite.Interpreter(model_path)
+    interpreter = tf.lite.Interpreter(model_path)
     interpreter.allocate_tensors()
     
     if source.endswith("jpg") or source.endswith("jpeg"):
         image = Image.open(source)
         new_image, time = detect_image(image, interpreter, imgsz, data, source, conf)
-        cv2.namedWindow("image", cv2.WINDOW_AUTOSIZE)
-        cv2.imshow("image", new_image)
-        cv2.waitKey(0)
+        # cv2.namedWindow("image", cv2.WINDOW_AUTOSIZE)
+        # cv2.imshow("image", new_image)
+        # cv2.waitKey(0)
         cv2.imwrite(source[:len(source)-4]+"Output.jpg", new_image)
     elif source.endswith("m4v") or source.endswith("mp4"): 
         video = source
